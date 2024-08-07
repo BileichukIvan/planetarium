ea57200dd59d2ad122e63d09bb2704e39dddcb35
ivanb <bileichuk.ivan@gmail.com>
2024-08-07 18:49:30 +0300

    fix: Deleted unused imports in planetarium/views

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,11 +1,8 @@
 from datetime import datetime
 
 from django.db.models import F, Count
-from rest_framework import viewsets, mixins, status
-from rest_framework.decorators import action
+from rest_framework import viewsets, mixins
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from planetarium.models import (
@@ -14,7 +11,6 @@ from planetarium.models import (
     PlanetariumDome,
     ShowSession,
     Reservation,
-    Ticket
 )
 from planetarium.serializers import (
     AstronomyShowSerializer,
@@ -26,9 +22,6 @@ from planetarium.serializers import (
     ShowSessionDetailSerializer,
     ReservationSerializer,
     ReservationListSerializer,
-    TicketSerializer,
-    TicketListSerializer,
-    TicketSeatsSerializer,
     PlanetariumDomeSerializer,
 )
 
